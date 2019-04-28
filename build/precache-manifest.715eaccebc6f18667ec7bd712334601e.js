10c8ffc24444a943f325e60d6b40700adc8f22eb
zackprod <eh.ziko.1998@gmail.com>
2019-04-28 03:28:27 +0100

    added files

---
+++ build/precache-manifest.715eaccebc6f18667ec7bd712334601e.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "a35720c2fed2c7f043bc7e4ffb45e073",
+    "url": "/static/media/fontawesome-webfont.a35720c2.woff"
+  },
+  {
+    "revision": "fe7883e90c308ad0465b",
+    "url": "/static/css/main.184d1f56.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "608fd10666a860567740497e84492928",
+    "url": "/static/media/summernote.608fd106.woff"
+  },
+  {
+    "revision": "9297c21cb972bc8f3706",
+    "url": "/static/js/2.47df7c7a.chunk.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "db812d8a70a4e88e888744c1c9a27e89",
+    "url": "/static/media/fontawesome-webfont.db812d8a.woff2"
+  },
+  {
+    "revision": "fe7883e90c308ad0465b",
+    "url": "/static/js/main.2dadb3f9.chunk.js"
+  },
+  {
+    "revision": "a3de2170e4e9df77161ea5d3f31b2668",
+    "url": "/static/media/fontawesome-webfont.a3de2170.ttf"
+  },
+  {
+    "revision": "32400f4e08932a94d8bfd2422702c446",
+    "url": "/static/media/fontawesome-webfont.32400f4e.eot"
+  },
+  {
+    "revision": "f775f9cca88e21d45bebe185b27c0e5b",
+    "url": "/static/media/fontawesome-webfont.f775f9cc.svg"
+  },
+  {
+    "revision": "c119d6f9c785911c6ec82b67fb45609d",
+    "url": "/static/media/adminpro-icon.c119d6f9.woff"
+  },
+  {
+    "revision": "92da6ace775dad8ae5d3a2f307499f68",
+    "url": "/static/media/adminpro-icon.92da6ace.svg"
+  },
+  {
+    "revision": "c0307ca7c13d2b77a303ee1fa6ff698e",
+    "url": "/static/media/adminpro-icon.c0307ca7.ttf"
+  },
+  {
+    "revision": "372a2830643e759ed61f6a8590900192",
+    "url": "/static/media/summernote.372a2830.eot"
+  },
+  {
+    "revision": "ce9ce49a0e1ca283fa242ba2cf8e24d9",
+    "url": "/static/media/summernote.ce9ce49a.ttf"
+  },
+  {
+    "revision": "9297c21cb972bc8f3706",
+    "url": "/static/css/2.dc85d0ce.chunk.css"
+  },
+  {
+    "revision": "2542e1ba41a33aad9fac68f7b05cbc54",
+    "url": "/index.html"
+  }
+];
